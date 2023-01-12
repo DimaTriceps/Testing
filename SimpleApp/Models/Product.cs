17b9e5fa7e718c83399ab69ec9919ee61b634fef
Dmitriy Listopad <dimalist2005@gmail.com>
2023-01-12 04:45:06 +0300

    Create Models folder within Product class

---
+++ SimpleApp/Models/Product.cs
@@ -0,0 +1,14 @@
+﻿namespace SimpleApp.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public static Product[] GetProducts()
+        {
+            Product kayak = new Product { Name = "Kayak", Price = 275M };
+            Product lifejacket = new Product { Name = "lifejacket", Price = 49M };
+            return new Product[] { kayak, lifejacket };
+        }
+    }
+}
