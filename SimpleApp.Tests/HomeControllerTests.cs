f21776592f5bab7bacfcdfecd89fd3103e21bb3a
Dmitriy Listopad <dimalist2005@gmail.com>
2023-01-13 07:26:27 +0300

    Add HomeControllerTests class

---
+++ SimpleApp.Tests/HomeControllerTests.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Xunit;
+using System.Collections.Generic;
+using SimpleApp.Controllers;
+using SimpleApp.Models;
+
+namespace SimpleApp.Tests
+{
+    public class HomeControllerTests
+    {
+        
+    }
+}
