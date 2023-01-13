3533152abed6f6a09b69dd3c108c59d968aaee9e
Dmitriy Listopad <dimalist2005@gmail.com>
2023-01-13 06:21:05 +0300

    Add CanChangeProductName method

--- SimpleApp.Tests/ProductTests.cs
+++ SimpleApp.Tests/ProductTests.cs
@@ -4,6 +4,12 @@ namespace SimpleApp.Tests
 {
     public class ProductTests
     {
-
+        [Fact]
+        public void CanChangeProductName()
+        {
+            var p = new Product { Name = "Test", Price = 100M};
+            p.Name = "New Name";
+            Assert.Equal("New Name", p.Name);
+        }
     }
 }
