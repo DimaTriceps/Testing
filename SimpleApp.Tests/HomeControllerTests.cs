cf6818c69237cf7cf73e5dde9f536370f646ad82
Dmitriy Listopad <dimalist2005@gmail.com>
2023-01-17 07:01:09 +0300

    Added HomeController unit tests

--- SimpleApp.Tests/HomeControllerTests.cs
+++ SimpleApp.Tests/HomeControllerTests.cs
@@ -8,6 +8,24 @@ namespace SimpleApp.Tests
 {
     public class HomeControllerTests
     {
-        
+        [Fact]
+        public void IndexActionModelIsComplete()
+        {
+            //Arrange
+            var controller = new HomeController();
+            Product[] products = new Product[]
+            {
+                new Product{Name="kayak", Price=275M },
+                new Product {Name="lifejacket", Price = 49M}
+            };
+
+            //Act
+            var model = (controller.Index() as ViewResult)?.ViewData.Model
+                as IEnumerable<Product>;
+            //Assert
+            Assert.Equal(products, model,
+                Comparer.Get<Product>((p1, p2) => p1.Name == p2.Name &&
+                    p1.Price == p2.Price));
+        }
     }
 }
