23ab8d4ba00161078af5c57836bde8fd590b2f26
Dmitriy Listopad <dimalist2005@gmail.com>
2023-01-18 10:35:26 +0300

    Add ProductDataSource

--- SimpleApp/Models/Product.cs
+++ SimpleApp/Models/Product.cs
@@ -4,11 +4,20 @@
     {
         public string Name { get; set; }
         public decimal? Price { get; set; }
-        public static Product[] GetProducts()
+        //public static Product[] GetProducts()
+        //{
+        //    Product kayak = new Product { Name = "Kayak", Price = 275M };
+        //    Product lifejacket = new Product { Name = "lifejacket", Price = 49M };
+        //    return new Product[] { kayak, lifejacket };
+        //}
+    }
+
+    public class ProductDataSource : IDataSource
+    {
+        public IEnumerable<Product> Products => new Product[]
         {
-            Product kayak = new Product { Name = "Kayak", Price = 275M };
-            Product lifejacket = new Product { Name = "lifejacket", Price = 49M };
-            return new Product[] { kayak, lifejacket };
-        }
+            new Product{Name = "kayak", Price = 275M},
+            new Product{Name = "Lifejacket", Price = 49M}
+        };
     }
 }
